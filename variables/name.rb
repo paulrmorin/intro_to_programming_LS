0faef68d2a0d6d39f56ecd573b91e7d4d3679ec1
Paul Morin <pmorin35@gmail.com>
2020-12-26 07:38:06 -0800

    add exercises from variables chapter.

---
+++ variables/name.rb
@@ -0,0 +1,4 @@
+# asks the user to type in their name and then prints out a greeting message with their name included.
+puts "What is your name?"
+name = gets.chomp
+puts "Nice to meet you " + name + "."
---
+++ variables/name2.rb
@@ -0,0 +1,8 @@
+# prints the name of the user 10 times.
+puts "What is your name?"
+name = gets.chomp
+# puts "Nice to meet you " + name + "."
+
+10.times do 
+  puts name
+end  
