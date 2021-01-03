fbefe936ae7aeaa2f640cadc72e3dd3ac4edbb6e
Paul Morin <pmorin35@gmail.com>
2021-01-03 08:02:12 -0800

    exercises from chapter on loops and iterators.

---
+++ loops_iterators/exercise2.rb
@@ -0,0 +1,5 @@
+answer = " "
+while answer != "stop"
+  puts "Where are you from? Say stop when you get bored."
+  answer = gets.chomp
+end  
---
+++ loops_iterators/exercise3.rb
@@ -0,0 +1,9 @@
+# a method that counts down to zero using recursion.
+
+def countdown(num)
+  puts num
+  if num > 0
+    countdown(num - 1)
+  end  
+end  
+   
